e9cc2e7efeec1014b522639a82f44226b5cb64da
Danny Fuhriman <danny.fuhriman@Okta.com>
2021-11-19 11:46:59 -0800

    refactor Feedback components

---
+++ src/components/atoms/Feedback/index.jsx
@@ -0,0 +1,7 @@
+/** @format */
+
+export * from './Alert';
+export * from './AuthDialog';
+export * from './Loader';
+export * from './LoadingOverlay';
+export * from './Snackbar';
--- src/providers/reducers/AuthReducer.jsx
+++ src/providers/reducers/AuthReducer.jsx
@@ -1,8 +1,13 @@
 /** @format */
 
+import * as _ from 'lodash';
+
 export const initialState = {
+	isError: false,
 	isLoading: false,
+	isLoadingLogin: false,
 	isAuthenticated: false,
+	isLoadingProfile: false,
 };
 
 export const AuthReducer = (state, action) => {
@@ -11,111 +16,144 @@ export const AuthReducer = (state, action) => {
 	// console.debug('=======    action     =======');
 	// console.debug(JSON.stringify(action, null, 2));
 	switch (action.type) {
+		case 'DISMISS_ERROR':
+			if (state?.error) {
+				delete state.error;
+			}
+			return _.merge({}, state, action?.payload, { isError: false });
 		case 'GET_FACTORS':
 		case 'REMOVE_FACTOR':
-			return { ...state, factorsAreLoading: true };
+			return _.merge({}, state, action?.payload, { factorsAreLoading: true });
 		case 'GET_USER':
-			return { ...state, profileIsLoading: true };
+			return _.merge({}, state, action?.payload, { isLoadingProfile: true });
+		case 'LOGIN_AUTHORIZE':
+		case 'LOGIN_STARTED':
+			return _.merge({}, state, action?.payload, {
+				iFrameIsVisible: true,
+				authModalIsVisible: true,
+				isLoadingLogin: false,
+			});
+		case 'LOGIN_START':
+			return _.merge({}, state, action?.payload, { isLoadingLogin: true });
+		case 'LOGIN_MODAL_START':
+			return _.merge({}, state, action?.payload, {
+				iFrameIsVisible: true,
+				authModalIsVisible: true,
+				isLoadingLogin: true,
+			});
+		case 'SILENT_AUTH_START':
+			return _.merge({}, state, action?.payload, {
+				isLoadingLogin: true,
+			});
+		case 'SILENT_AUTH_END':
+			return _.merge({}, state, action?.payload, {
+				isLoadingLogin: false,
+				authModalIsVisible: false,
+				iFrameIsVisible: false,
+			});
 		case 'STEP_UP_STARTED':
-			return {
-				...state,
-				...action?.payload,
-				iFrameIsVisible: true,
-				authModalIsVisible: true,
-				isLoading: false,
-			};
+			return _.merge({}, state, action?.payload, {
+				isLoadingLogin: false,
+				authModalIsVisible: false,
+				iFrameIsVisible: false,
+			});
 		case 'STEP_UP_START':
-			return {
-				...state,
-				...action?.payload,
-				isLoading: true,
-			};
+			return _.merge({}, state, action?.payload, {
+				isLoadingLogin: true,
+			});
 		case 'STEP_UP_MODAL_START':
-			return {
-				...state,
-				...action?.payload,
-				iFrameIsVisible: true,
+			return _.merge({}, state, action?.payload, {
+				isLoadingLogin: true,
 				authModalIsVisible: true,
-				isLoading: true,
-			};
+				iFrameIsVisible: true,
+			});
 		case 'LOGIN_REDIRECT':
-		case 'LOGIN_WITH_CREDENTIALS':
-		case 'LOGIN':
-			return { ...state, ...action?.payload, isLoading: true };
-		// case 'INITIALIZE':
-		// return { ...state, ...action?.payload, isLoading: true };
-		case 'REFRESH_FACTORS':
-			let resp = {
-				...state,
-				...action?.payload,
-				factorsAreLoading: true,
-				isStale: true,
-			};
-			// console.debug(JSON.stringify(resp, null, 2));
-			return resp;
-		case 'MFA_ENROLL_SUCCESS':
-			return {
-				...state,
-				...action?.payload,
-				isLoading: false,
-				isStale: true,
-			};
-		case 'STEP_UP_COMPLETE':
-			return {
-				...state,
-				isStale: false,
-				...action?.payload,
-				isLoading: true,
+			return _.merge({}, state, action?.payload, {
+				isLoadingLogin: true,
+			});
+		case 'EXCHANGE_CODE':
+		case 'LOGIN_COMPLETE':
+			return _.merge({}, state, action?.payload, {
+				isLoadingLogin: true,
 				iFrameIsVisible: false,
 				authModalIsVisible: false,
-			};
+			});
+
+		case 'LOGIN_WITH_CREDENTIALS':
+		case 'LOGIN':
+			return _.merge({}, state, action?.payload, { isLoadingLogin: true });
+		case 'REFRESH_FACTORS':
+			return _.merge({}, state, action?.payload, {
+				factorsAreLoading: true,
+				isStale: true,
+			});
+		case 'MFA_ENROLL_SUCCESS':
+			return _.merge({}, state, action?.payload, {
+				isLoading: false,
+				isStale: true,
+			});
+		case 'STEP_UP_COMPLETE':
+			return _.merge({}, state, { isStale: false }, action?.payload, {
+				isLoading: false,
+				iFrameIsVisible: false,
+				authModalIsVisible: false,
+			});
 		case 'STEP_UP_REQUIRED_FIDO':
 			state = { ...state, fidoMFA: true };
 		// eslint-disable-next-line no-fallthrough
 		case 'STEP_UP_REQUIRED':
-			return {
-				...state,
-				...action?.payload,
+			return _.merge({}, state, action?.payload, {
 				isLoading: false,
 				isStepUpRequired: true,
-			};
+			});
 		case 'STEP_UP_SUCCESS':
-			state = {
-				...state,
-				isStepUpRequired: false,
-				fidoMFA: false,
+			state = _.merge({}, state, action?.payload, {
 				isLoading: false,
-			};
+				isStepUpRequired: true,
+				fidoMFA: false,
+			});
 		// eslint-disable-next-line no-fallthrough
 		case 'GET_USER_SUCCESS':
+		case 'SILENT_AUTH_SUCCESS':
+		case 'SILENT_AUTH_CANCEL':
 		case 'AUTHN_SUCCESS':
 		case 'IDX_NEXT':
+		case 'LOGIN_SUCCESS':
+			delete state.tokenParams;
+			return _.merge({}, state, { isStale: true }, action?.payload, {
+				isLoadingLogin: false,
+			});
 		case 'SUCCESS':
-			return {
-				...state,
-				isStale: true,
-				...action?.payload,
+			return _.merge({}, state, action?.payload, {
 				isLoading: false,
-			};
-		case 'LOGIN_CANCEL':
-			state.idxModalIsVisible = false;
+			});
 		case 'STEP_UP_CANCEL':
-			return {
-				...state,
-				...action?.payload,
-				isLoading: false,
+		case 'LOGIN_CANCEL':
+			return _.merge({}, state, action?.payload, {
+				isLoadingLogin: false,
 				authModalIsVisible: false,
-			};
+				iFrameIsVisible: false,
+				idxModalIsVisible: false,
+			});
 		case 'LOGOUT_SUCCESS':
-			return { ...state, ...action?.payload, isLoading: false };
+			return _.merge({}, state, action?.payload, { isLoadingLogout: false });
 		case 'LOGOUT':
-			return { ...state, ...action?.payload, isLoading: true };
+			return _.merge({}, state, action?.payload, { isLoadingLogout: true });
 		case 'MFA_ERROR':
 		case 'STEP_UP_ERROR':
 		case 'FETCH_ERROR':
 		case 'LOGIN_ERROR':
-			return { ...state, errorMessage: action?.error };
+			let result = _.merge({}, state, initialState, action?.payload, {
+				error: action?.error,
+				isError: true,
+			});
+			if (action?.error) {
+				console.error(action.error);
+			}
+			return result;
 		default:
-			throw new Error(`Unhandled action type: ${action.type}`);
+			let error = new Error(`Unhandled action type: ${action.type}`);
+			console.error(error);
+			throw error;
 	}
 };
