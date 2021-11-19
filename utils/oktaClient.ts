5b32e846e51ad17d1934251a6e52b5be09920428
Danny Fuhriman <danny.fuhriman@Okta.com>
2021-11-19 14:57:10 -0800

    remove unused variable

--- utils/oktaClient.ts
+++ utils/oktaClient.ts
@@ -3,7 +3,7 @@
 import * as okta from '@okta/okta-sdk-nodejs';
 
 const API_KEY = process.env.API_OKTA_KEY;
-const CLIENT_ID = process.env.API_OKTA_CLIENT_ID;
+// const CLIENT_ID = process.env.API_OKTA_CLIENT_ID;
 export const ORG_URL = 'https://expedia-oie.dannyfuhriman.com';
 
 export const client = new okta.Client({
