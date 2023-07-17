225871df5d7e47d781f9669923194a74437065ab
Danny Fuhriman <danny.fuhriman@Okta.com>
2023-07-17 17:13:47 -0600

    fix auth config

--- src/config/authConfig.js
+++ src/config/authConfig.js
@@ -1,14 +1,17 @@
 /** @format */
 
-const OKTA_TESTING_DISABLEHTTPSCHECK =
-	process.env.OKTA_TESTING_DISABLEHTTPSCHECK || false;
-const REDIRECT_URI = `${window.location.origin}/login/callback`;
-const SCOPES = process.env.REACT_APP_OKTA_SCOPES;
+import { OKTA_URL } from '../../utils';
+
+const {
+	OKTA_TESTING_DISABLEHTTPSCHECK = false,
+	REACT_APP_REDIRECT_URI: REDIRECT_URI = `${window.location.origin}/login/callback`,
+	REACT_APP_OKTA_SCOPES: SCOPES,
+	REACT_APP_OKTA_CLIENT_ID: CLIENT_ID,
+	REACT_APP_OKTA_AUTH_SERVER_ID: AUTH_SERVER_ID,
+} = process.env;
+
 // const SCOPES = 'openid profile email';
-const CLIENT_ID = process.env.REACT_APP_OKTA_CLIENT_ID;
 // const CLIENT_ID = '0oa120pjs4hOYsSJx0h8';
-const AUTH_SERVER_ID = process.env.REACT_APP_OKTA_AUTH_SERVER_ID;
-const OKTA_URL = process.env.REACT_APP_OKTA_URL;
 const ISSUER = `${OKTA_URL}/oauth2/${AUTH_SERVER_ID}`;
 // const ISSUER = 'https://expedia.dannyfuhriman.com/oauth2/default';
 // const ISSUER = 'https://udp-expedia-oie.oktapreview.com/oauth2/aus1gb3zbtlqNoCUK1d7';
